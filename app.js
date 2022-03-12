515df6735ebaf3c67a57f6f11071ad7a9679c074
Lingadurai <89645684+LingaduraiP@users.noreply.github.com>
2022-03-12 15:06:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+console.log(navigator);
+// async function selectMediaStream() {
+//   try {
+//     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+//     videoElement.srcObject = mediaStream;
+//     videoElement.onloadedmetadata = () => {
+//       videoElement.play();
+//     };
+//   } catch (error) {
+//     console.log("some Error is there : ", error);
+//   }
+// }
+// button.addEventListener("click", async () => {
+//   //buttion disable
+//   button.disabled = true;
+
+//   //Start Picture in Picture
+//   await videoElement.requestPictureInPicture();
+
+//   //   Reset Button
+//   button.disabled = false;
+// });
+// selectMediaStream();
